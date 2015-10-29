3b1f440d1ffe0c8aedaa90f1aea9caf9c251e720
antoinestevens <antoinexstevens@gmail.com>
2015-10-29 14:11:06 +0100

    change cores to cl option

--- R/compare_copernicus.R
+++ R/compare_copernicus.R
@@ -3,20 +3,22 @@
 #' x (lon), y (lat), z (time). The analysis is performed pixel by pixel. The temporal profile (z dimension) of each
 #' pixel at a given (x,y) is extracted in the two raster and compared using a set of statistics. This results in a raster map for
 #' each computed statistic, depicting the spatial variability of the temporal coherence (agreement) between the two datasets.
-#' @usage compare_raster_time(x,y,stats,FUN = NULL,filename=rasterTmpFile(),cores,...)
+#' @usage compare_raster_time(x,y,stats,FUN = NULL,cl = NULL,filename=rasterTmpFile(),...)
 #' @param x A \code{\link[raster]{Raster-class}} object. Better be a \code{RasterBrick}, for faster computing
 #' @param y Another \code{\link[raster]{Raster-class}} object to compare with the first one
-#' @param stats A character \code{vector} with one or more of the following: 'cor',
-#' 'ax', 'ay', 'bx', 'by', 'ac','acu', 'acs', 'mbe', 'rmsd', 'rmspd',  'rmpdu', 'rmpds','mpdpu','mpdps'. See details
+#' @param stats A character \code{vector} with one or more of the following: 'missing', 'cor',
+#' 'ax', 'ay', 'bx', 'by', 'ac','acu', 'acs', 'mbe', 'rmsd', 'rmspd',  'rmpdu', 'rmpds','mpdpu','mpdps',
+#' 'smoothness','atime','btime'. See details
 #' @param FUN An optional function to be applied on the \code{z} slot of the input \code{x} and \code{y} (can be set with
 #' \code{\link[raster]{setZ}}, to group statistics according to the resulting classes. If provided, only one
 #' \code{stats} is allowed. See Examples section.
-#' @param cores number of cores used when computing statistics. Default is 1.
+#' @param cl cluster object for parallel processing. Default is \code{NULL}
 #' @param filename name of the file where the output raster should be saved. Default is created through \code{\link[raster]{rasterTmpFile}}
 #' @param ... arguments passed to \code{\link[raster]{writeRaster}}
 #' @details
 #' The \code{stats} arguments can take one or more of the following values (see Ji and Gallo, 2006):
 #' \itemize{
+#'    \item{\code{missing}}{ Number of missing values in the \code{x} and \code{y} raster}
 #'    \item{\code{cor}}{ Correlation coefficient}
 #'    \item{\code{ax}}{ Intercept of the Geometric Mean Functional Relationship (GMFR) model x = a + by}
 #'    \item{\code{ay}}{ Intercept of the GMFR model y = a + bx}
@@ -32,6 +34,10 @@
 #'    \item{\code{rmpds}}{ Systematic Square Root of Mean Product-difference}
 #'    \item{\code{mpdpu}}{ Proportion of the unsystematic differences over the total difference}
 #'    \item{\code{mpdps}}{ Proportion of the systematic differences over the total difference}
+#'    \item{\code{smoothness}}{ Mean absolute value of the first derivative over time}
+#'    \item{\code{atime}}{ Intercept of the regression over time}
+#'    \item{\code{btime}}{ Coefficient of the regression over time}
+
 #' }
 #' @return A \code{\link[raster]{Raster-class}} object with layers correponding to \code{stats}. If \code{FUN} argument
 #' is provided, layers correponds to the groups resulting from applying \code{FUN} to the \code{z} slot of input rasters \code{x}
@@ -65,7 +71,7 @@
 #' PROBA  <- writeRaster(stack(f_PROBA),filename = rasterTmpFile())
 #'
 #' # Compare temporal consistency
-#' # Use the 'cores' to speed up processing!
+#' # Use the 'cl' option to speed up processing!
 #' ct <- compare_raster_time(SPOT,PROBA)
 #' # plot
 #' brks <- c(0,.05,.1,.2,.3,.7)
@@ -95,10 +101,34 @@
 #'
 #' @export
 compare_raster_time <- function(x,y,
-                                stats = c("cor", "ax", "ay", "bx", "by", "ac","acu", "acs", "mbe", "rmsd", "rmspd",  "rmpdu", "rmpds","mpdpu","mpdps"),
-                                FUN = NULL,filename=rasterTmpFile(),cores,...){
+                                stats = c("missing", "cor", "ax", "ay", "bx", "by", "ac","acu", "acs", "mbe",
+                                          "rmsd", "rmspd",  "rmpdu", "rmpds","mpdpu","mpdps",
+                                          "smoothness","atime","btime"),
+                                FUN = NULL,cl = NULL,filename=rasterTmpFile(),...){
+
+  if(!is.null(cl))
+    if(!"cluster"%in%class(cl))
+      stop("cl should be a cluster object. See ?getCluster")
 
   stats <- match.arg(stats,several.ok = TRUE)
+
+  # change stats
+  if("missing"%in%stats){
+    stats <- stats[!stats%in%"missing"]
+    stats <- c(stats,paste("missing",c("x","y"),sep="_"))
+  }
+  if("smoothness"%in%stats){
+    stats <- stats[!stats%in%"smoothness"]
+    stats <- c(stats,paste("smoothness",c("x","y"),sep="_"))
+  }
+  if("atime"%in%stats){
+    stats <- stats[!stats%in%"atime"]
+    stats <- c(stats,paste("atime",c("x","y"),sep="_"))
+  }
+  if("btime"%in%stats){
+    stats <- stats[!stats%in%"btime"]
+    stats <- c(stats,paste("btime",c("x","y"),sep="_"))
+  }
 
   if(!inherits(x,"Raster"))
     x <- stack(x, quick=TRUE)
@@ -131,19 +161,25 @@ compare_raster_time <- function(x,y,
     nl <- length(stats)
     fn <- stats
   }
+
+  if(any(stats %in% c("atime_x","btime_x"))){
+    z <- getZ(x)
+    if(!length(z))
+      stop("Set z values via 'setZ'")
+  } else {
+    z <- NULL
+  }
+
   b <- list()
   b[[1]] <- brick(x,nl=nl, values=FALSE)
   b[[1]] <- writeStart(b[[1]], filename = filename,...)
-  if (missing(cores)){
+  if (is.null(cl)){
     tr <- blockSize(x,n = nlayers(x)*2)
     for ( i in seq_along(tr$row))
-      b[[1]] <- writeValues(b[[1]], .compare_time(i = i, x = x, y = y, row = tr$row, nrows = tr$nrow, stats = stats, f = f), tr$row[i])
+      b[[1]] <- writeValues(b[[1]], .compare_time(i = i, x = x, y = y, z = z, row = tr$row, nrows = tr$nrow, stats = stats, f = f), tr$row[i])
   } else {
 
-    beginCluster(cores)
-    cl <- getCluster()
-    on.exit(endCluster())
-
+    cores <- length(cl)
     # send expr and data to cluster nodes
     snow::clusterEvalQ(cl,library(matrixStats))
     snow::clusterExport(cl,".compare_xy")
@@ -152,7 +188,7 @@ compare_raster_time <- function(x,y,
     tr <- blockSize(x, minblocks=cores)
 
     for (i in 1:cores)
-      snow::sendCall(cl[[i]],.compare_time,list(i = i, x = x, y = y, row = tr$row, nrows = tr$nrow, stats = stats, f = f),tag=i)
+      snow::sendCall(cl[[i]],.compare_time,list(i = i, x = x, y = y, z = z, row = tr$row, nrows = tr$nrow, stats = stats, f = f),tag=i)
 
     for (i in 1:tr$n) {
       d <- snow::recvOneData(cl);
@@ -162,7 +198,7 @@ compare_raster_time <- function(x,y,
       b[[1]] <- writeValues(b[[1]], d$value$value, tr$row[d$value$tag])
       ni <- cores + i
       if (ni <= tr$n)
-        snow::sendCall(cl[[d$node]],.compare_time,list(i = ni, x = x, y = y, row = tr$row, nrows = tr$nrow, stats = stats, f = f),tag=ni)
+        snow::sendCall(cl[[d$node]],.compare_time,list(i = ni, x = x, y = y, z = z, row = tr$row, nrows = tr$nrow, stats = stats, f = f),tag=ni)
     }
   }
 
@@ -174,18 +210,18 @@ compare_raster_time <- function(x,y,
   b
 }
 
-.compare_time <- function(i,x,y,row,nrows, stats, f){
+.compare_time <- function(i,x,y,z,row,nrows, stats, f){
   x <- getValues(x,row=row[i], nrows=nrows[i])
   y <- getValues(y,row=row[i], nrows=nrows[i])
   if(is.null(f)){
-    .compare_xy(x,y,stats)
+    .compare_xy(x,y,z,stats)
   } else {
     res <- matrix(NA,nrow = nrow(x),ncol = nlevels(factor(f)))
     for(k in 1:nlevels(factor(f))){
       id <-  factor(f) == levels(factor(f))[k]
       xk <- x[,id]
       yk <- y[,id]
-      res[,k] <- .compare_xy(xk,yk,stats)
+      res[,k] <- .compare_xy(xk,yk,z,stats)
     }
     res
   }
@@ -201,10 +237,11 @@ compare_raster_time <- function(x,y,
 #' @param x A \code{\link[raster]{Raster-class}} object. Better be a \code{RasterBrick}, for faster computing
 #' @param y Another \code{\link[raster]{Raster-class}} object to compare with the first one
 #' @param lc An optional \code{\link[raster]{raster}} object giving classes for which separate statistics are retrieved
-#' @param stats A character \code{vector} with one or more of the following: 'cor',
+#' @param stats A character \code{vector} with one or more of the following: 'missing', 'cor',
 #' 'ax', 'ay', 'bx', 'by', 'ac','acu', 'acs', 'mbe', 'rmsd', 'rmspd',  'rmpdu', 'rmpds','mpdpu','mpdps'. See details
 #' The \code{stats} arguments can take one or more of the following values (see Ji and Gallo, 2006):
 #' \itemize{
+#'    \item{\code{missing}}{ Number of missing values in the \code{x} and \code{y} raster}
 #'    \item{\code{cor}}{ Correlation coefficient}
 #'    \item{\code{ax}}{ Intercept of the Geometric Mean Functional Relationship (GMFR) model x = a + by}
 #'    \item{\code{ay}}{ Intercept of the GMFR model y = a + bx}
@@ -270,8 +307,15 @@ compare_raster_time <- function(x,y,
 #'
 #' @export
 compare_raster_space <- function(x,y,lc,
-                                 stats= c("cor", "ax", "ay", "bx", "by", "ac", "acu", "acs", "mbe" ,"rmsd", "rmspd", "rmpdu", "rmpds","mpdpu","mpdps")){
+                                 stats= c("missing", "cor", "ax", "ay", "bx", "by", "ac", "acu", "acs", "mbe" ,
+                                          "rmsd", "rmspd", "rmpdu", "rmpds","mpdpu","mpdps")){
   stats <- match.arg(stats,several.ok = TRUE)
+
+  # change stats
+  if("missing"%in%stats){
+    stats <- stats[!stats%in%"missing"]
+    stats <- c(stats,paste("missing",c("x","y"),sep="_"))
+  }
 
   if(!inherits(x,"Raster"))
     x <- stack(x, quick=TRUE)
@@ -282,15 +326,17 @@ compare_raster_space <- function(x,y,lc,
   if(!missing(lc)){
     if(!inherits(lc,"Raster"))
       lc <- stack(lc, quick=TRUE)
-    z <- factor(getValues(lc))
-    res <- array(NA,dim = c(nlayers(x),length(stats),nlevels(z)))
-    dimnames(res) <- list(names(x),stats,levels(z))
+    lc.levels <- factor(getValues(lc))
+    res <- array(NA,dim = c(nlayers(x),length(stats),nlevels(lc.levels)))
+    dimnames(res) <- list(names(x),stats,levels(lc.levels))
   } else {
-    z <- NULL
+    lc.levels <- NULL
     res <- matrix(NA,nrow = nlayers(x),ncol = length(stats))
     rownames(res) <- names(x)
     colnames(res) <- stats
   }
+
+  z <- NULL
 
   # compare dimensions
   if(!compareRaster(x,y))
@@ -310,19 +356,19 @@ compare_raster_space <- function(x,y,lc,
     xx <- getValuesBlock(x,row = 1, nrows = nrow(x),col = 1, ncols = ncol(x),lyrs = ch)
     yy <- getValuesBlock(y,row = 1, nrows = nrow(y),col = 1, ncols = ncol(y),lyrs = ch)
 
-    if(is.null(z)){
-      res[ch,] <- .compare_xy(xx,yy,stats,byrow = FALSE)
+    if(is.null(lc.levels)){
+      res[ch,] <- .compare_xy(xx,yy,z,stats,byrow = FALSE)
     } else {
-      for(k in 1:nlevels(z)){
-        id <-  z == levels(z)[k]
-        res[ch,,k] <- .compare_xy(xx[id,],yy[id,],stats,byrow = FALSE)
+      for(k in 1:nlevels(lc.levels)){
+        id <-  lc.levels == levels(lc.levels)[k]
+        res[ch,,k] <- .compare_xy(xx[id,],yy[id,],z,stats,byrow = FALSE)
       }
     }
   }
   res
 }
 
-.compare_xy <- function(x,y,stats,byrow = TRUE){
+.compare_xy <- function(x,y,z,stats,byrow = TRUE){
   # See Ji and Gallo (2006) An Agreement Coefficient for Image Comparison
   # pg 826 ex:
   # x = c(6, 8, 9, 10, 11, 14); y = c(2, 3, 5, 5, 6, 8)
@@ -330,10 +376,41 @@ compare_raster_space <- function(x,y,lc,
     x <- t(x)
     y <- t(y)
   }
-
   py <- ncol(y);ny <- nrow(y) # dims
   res <- matrix(nrow = ny, ncol = length(stats))
   colnames(res) <- stats
+
+  if("missing_x" %in% stats){
+    mix <- matrixStats::rowCounts(x,value = NA)/py*100 # much faster
+    miy <- matrixStats::rowCounts(y,value = NA)/py*100
+    res[,"missing_x"] <- mix
+    res[,"missing_y"] <- miy
+  }
+
+  if("smoothness_x" %in% stats){
+    difx <- matrixStats::rowDiffs(x) # derivative
+    difx <- rowMeans(abs(difx),na.rm=T)
+    # difx <- rowMeans(abs(t(t(difx)/as.numeric(diff(z)))),na.rm=T) # this is the correct derivative
+    dify <- matrixStats::rowDiffs(y) # derivative
+    dify <- rowMeans(abs(dify),na.rm=T)
+    res[,"smoothness_x"] <- difx
+    res[,"smoothness_y"] <- dify
+  }
+
+  if(any(stats %in% c("atime_x","btime_x"))){
+    ## add 1 for a model with an intercept
+    z <- cbind(1, (z - min(z))/365.25) # to get yearly coeff
+    abx <-  t(apply(x,1,function(y){ia <- !is.na(y);lm.fit(y = y[ia],x=z[ia,])$coefficient}))
+    aby <-  t(apply(y,1,function(y){ia <- !is.na(y);lm.fit(y = y[ia],x=z[ia,])$coefficient}))
+    if("atime_x" %in% stats){
+      res[,"atime_x"] <- abx[,1]
+      res[,"atime_y"] <- aby[,1]
+    }
+    if("btime_x" %in% stats){
+      res[,"btime_x"] <- abx[,2]
+      res[,"btime_y"] <- aby[,2]
+    }
+  }
 
   # keep only complete cases
   x[is.na(y)] <- NA
